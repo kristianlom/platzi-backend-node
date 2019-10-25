8fd9eae1b040dbe722469ce76ef6310101f795cf
Kristian Lopez <kristian.lom@gmail.com>
2019-10-24 23:13:24 -0500

    Se resuelve el reto del año bisiesto.

--- movies-api/index.js
+++ movies-api/index.js
@@ -10,6 +10,14 @@ app.get('/json', function(req, res) {
   res.send({ hello: 'world' });
 });
 
+app.get('/:year', (req, res) => {
+  let year = req.params.year;
+  res.send(
+    { bisiesto:
+        ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0))
+    });
+});
+
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
 });
