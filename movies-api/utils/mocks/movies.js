6d99daa593d3695513e11d53a116ebd117fc81c2
Kristian Lopez <kristian.lom@gmail.com>
2019-10-29 09:25:56 -0600

    Creación de tests para nuestros servicios.

--- movies-api/utils/mocks/movies.js
+++ movies-api/utils/mocks/movies.js
@@ -154,7 +154,7 @@ const moviesMock = [
 ];
 
 function filteredMoviesMock(tag) {
-  return moviesMock.filter(movie => movie.tags.include(tag));
+  return moviesMock.filter(movie => movie.tags.includes(tag));
 }
 
 class MoviesServiceMock {
@@ -165,7 +165,6 @@ class MoviesServiceMock {
   async createMovie() {
     return Promise.resolve(moviesMock[0]);
   }
-
 }
 
 module.exports = {
